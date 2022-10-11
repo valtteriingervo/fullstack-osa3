d5c3dd4a90b764a33adcc33a11ccbf38bcedf172
Valtteri Ingervo <valtteri.ingervo@aalto.fi>
2022-10-11 11:10:45 +0300

    3.5

--- index.js
+++ index.js
@@ -71,6 +71,32 @@ app.delete('/api/persons/:id', (request, response) => {
     response.status(204).end()
 })
 
+// Add new persons
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+
+    if (!body.name || !body.number) {
+        return response.status(400).json({
+            error: 'name or number is missing'
+        })
+    }
+
+    const person = {
+        id: getRandomID(),
+        name: body.name,
+        number: body.number
+    }
+
+    persons = persons.concat(person)
+
+    response.json(person)
+})
+
+
+const getRandomID = () => {
+    return Math.floor(Math.random() * 10000);
+}
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
