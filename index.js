f657aab95f69715dc6887605597c73e0ac70f5d8
Valtteri Ingervo <valtteri.ingervo@aalto.fi>
2022-10-21 17:21:36 +0300

    3.19

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ app.delete('/api/persons/:id', (request, response, next) => {
 })
 
 // POST new person
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
 
     if (body.name === undefined || body.name === undefined) {
@@ -84,9 +84,11 @@ app.post('/api/persons', (request, response) => {
         number: body.number,
     })
 
-    person.save().then(savedPerson => {
-        response.json(savedPerson)
-    })
+    person.save()
+        .then(savedPerson => {
+            response.json(savedPerson)
+        })
+        .catch(error => next(error))
 })
 
 // PUT -> Change person information
@@ -116,6 +118,9 @@ const errorHandler = (error, request, response, next) => {
 
     if (error.name === 'CastError') {
         return response.status(400).send({ error: 'malformatted id' })
+    }
+    else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
     }
 
     next(error)
